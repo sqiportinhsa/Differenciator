edff147e27d8efc4c4fc4afe33b93466dbd385e3
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2022-11-27 21:42:52 +0300

    add common

---
+++ common.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#include "common.h"
+
+#include "Tree/tree.h"
+#include "Rec_desc/descent.h"
+#include "Libs/file_reading.hpp"
+
+
+#define memory_allocate(ptr, size, type, returning)                                           \
+        ptr = (type*) calloc(size, sizeof(type));                                             \
+        if (ptr == nullptr) {                                                                 \
+            return returning;                                                                 \
+        }
+
+bool init_expression(Expression *expr, CLArgs *filenames) {
+    
+    assert(expr      != nullptr);
+    assert(filenames != nullptr);
+
+    expr->input_fn  = filenames->input;
+    expr->output_fn = filenames->output;
+
+    expr->text_origin = get_input(filenames->input);
+
+    init_tree(&expr->origin);
+    init_tree(&expr->diffirenciated);
+
+    expr->origin.head = create_orphan_node();
+
+    if (!descent(expr->text_origin, expr->origin.head)) {
+
+        return false;
+    }
+
+    return true;
+}
+
+CLArgs get_filenames(int argc, const char **argv) {
+
+    CLArgs args = parse_cmd_line(argc, argv);
+
+    if (args.input == nullptr) {
+
+        args.input = default_input;
+    }
+
+    if (args.output == nullptr) {
+
+        args.output = default_output;
+    }
+
+    return args;
+}
+
+void expr_dtor(Expression *expr) {
+
+    free(expr->text_origin);
+
+    tree_dtor(&expr->origin);
+    tree_dtor(&expr->diffirenciated);
+}
+
+static char* get_input(const char *filename) {
+
+    size_t len = count_elements_in_file(filename);
+
+    char *input = nullptr;
+
+    memory_allocate(filename, len, char, nullptr);
+
+    read_file(input, len, filename);
+
+    return input;
+}
---
+++ common.h
@@ -0,0 +1,24 @@
+#ifndef DIF_COMMON_H
+#define DIF_COMMON_H
+
+#include "Tree/tree.h"
+#include "Libs/file_reading.hpp"
+
+const char *default_input  = "example.txt";
+const char *default_output = "GDZ_matematica_5_class.pdf";
+
+struct Expression {
+    Tree  origin          = {};
+    Tree  diffirenciated  = {};
+    char* text_origin     = nullptr;
+    const char* input_fn  = nullptr;
+    const char* output_fn = nullptr;
+};
+
+bool init_expression(Expression *expr, CLArgs *filenames);
+
+CLArgs get_filenames(int argc, const char **argv);
+
+void expr_dtor(Expression *expr);
+
+#endif
