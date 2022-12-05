e2affcd23ac440bb560eaf847eee7a596203c631
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2022-12-05 05:02:41 +0300

    upd simplifying to dsl

--- DSL.h
+++ DSL.h
@@ -18,6 +18,13 @@
         latex_print_diff(source, dest);     \
         return dest;
 
+//------------------------------------------------------------------------------------------------//
+//                                                                                                //
+//                                        NODE CREATING                                           //
+//                                                                                                //
+//------------------------------------------------------------------------------------------------//
+
+
 #define Const(c) create_node(VAL, c)
 
 #define Add(l, r) create_node(OP, ADD, l, r)
@@ -43,5 +50,42 @@
         set_as_parent(dest);                \
         latex_print_diff(source, dest);     \
         return dest;
+
+//------------------------------------------------------------------------------------------------//
+//                                                                                                //
+//                                        SIMPLIFYING                                             //
+//                                                                                                //
+//------------------------------------------------------------------------------------------------//
+
+#define SKIP_NEUTRAL_ELEMENT(checking, brother, elem)                              \
+        if (checking->type == VAL && checking->data.val == elem) {                 \
+        Tree_node *simplifyed = brother;                                           \
+                                                                                   \
+        latex_print_diff(node, brother);                                           \
+                                                                                   \
+        free(checking);                                                            \
+        free(node);                                                                \
+                                                                                   \
+        return simplifyed;                                                         \
+    }
+
+#define REPLACE_TWO_VALUES(op_code, oper)                                                          \
+        if (node->left->type == VAL && node->right->type == VAL) {                                 \
+        	if (node->data.op == op_code) {                                                        \
+				Tree_node *new_node = Const(node->left->data.val oper node->right->data.val);      \
+				latex_print_diff(node, new_node);                                                  \
+				free_node(node);                                                                   \
+				return new_node;                                                                   \
+        	}                                                                                      \
+    	}
+
+#define REPLACE_VALUE_ON_ARG_TO(arg, value)               \
+    	if (node->left->data.val == arg) {                \
+			Tree_node *new_node = Const(value);           \
+    	    latex_print_diff(node, new_node);             \
+    	    free_node(node);                              \
+    	    return new_node;                              \
+    	}                                                 \
+    	return node;
 
 #endif
