dabd399d1185ce745be3c582411421194f285d23
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2022-12-05 06:33:58 +0300

    add introduction

--- Sol_generating/latex.h
+++ Sol_generating/latex.h
@@ -16,3 +16,5 @@ void latex_print_expr(Tree_node *head);
 void latex_print_diff(const Tree_node *orig, const Tree_node *diff);
 
 void latex_print_phrase();
+
+void print_diff_introduction(Tree *tree);
