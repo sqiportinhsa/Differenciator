a6f142e815b97e81c80bb6b113e0e0b1dc7291c3
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2022-12-10 18:26:29 +0300

    upd for transformations structure

--- Diff/diff.cpp
+++ Diff/diff.cpp
@@ -12,12 +12,10 @@
 
 #include "../DSL.h"
 
-static Tree_node* diff_node(Tree_node *source, Transformation **transf);
-
-static Tree_node* copy_subtree(Tree_node *source);
+static Tree_node* diff_node(Tree_node *source, Transformations *transf);
 
 
-#define DEBUG
+//#define DEBUG
 
 #ifdef DEBUG
 
@@ -33,7 +31,7 @@ static Tree_node* copy_subtree(Tree_node *source);
 
 
 #define memory_allocate(ptr, size, type)                                                      \
-        type *ptr = (type*) calloc(size, sizeof(type));                                       \
+        ptr = (type*) calloc(size, sizeof(type));                                             \
         if (ptr == nullptr) {                                                                 \
             printf("can't allocate memory: not enought free mem\n");                          \
             return;                                                                           \
@@ -43,11 +41,22 @@ void diff_tree(Tree *source, Tree *dest) {
 
     print_to_latex("\\section{Дифференцирование}");
 
-    int transfs_amount = count_nodes(source->head);
+    int transfs_amount = count_nodes(source->head->left);
+
+    printf("\n\n\n%d\n\n\n", transfs_amount);
+
+    Transformations transfs = {};
     
-    memory_allocate(transfs, transfs_amount, Transformation);
+    memory_allocate(transfs.orig, transfs_amount, Tree_node*);
+    memory_allocate(transfs.diff, transfs_amount, Tree_node*);
 
     dest->head->left = diff_node(source->head->left, &transfs);
+
+    make_replacings(source, dest);
+
+    latex_print_differenciation(&transfs, transfs_amount);
+
+    free_transfs(&transfs);
 
     print_to_latex("Таким образом получаем следующую производную:\n\n");
 
@@ -59,28 +68,23 @@ void diff_tree(Tree *source, Tree *dest) {
 
 #undef memory_allocate
 
-static Tree_node* diff_node(Tree_node *source, Transformation **transf) {
+static Tree_node* diff_node(Tree_node *source, Transformations *transf) {
 
     assert(source != nullptr);
 
     Tree_node *dest = nullptr;
 
     if (source->type == VAL) {
-    
         DEBUG_PRINT("diff value node.    val: %d\n", source->data.val);
-
         return Diff_const;
     }
 
     if (source->type == VAR) {
-
         DEBUG_PRINT("diff variable node. var: %d\n", source->data.var);
-
         return Diff_var;
     }
 
     DEBUG_PRINT("diff operation node. op: %d\n", source->data.op);
-    dump_subtree(source, "Subtree to diffirenciate:");
 
     switch (source->data.op) {
         case ADD: Ret_dest(Add(DL, DR));
@@ -101,24 +105,4 @@ static Tree_node* diff_node(Tree_node *source, Transformation **transf) {
     }
 
     return nullptr;
-}
-
-static Tree_node* copy_subtree(Tree_node *source) {
-
-    Tree_node *copy = create_empty_node();
-
-    copy->data = source->data;
-    copy->type = source->type;
-
-    if (source->left) {
-
-        copy->left  = copy_subtree(source->left);
-    }
-
-    if (source->right) {
-
-        copy->right = copy_subtree(source->right);
-    }
-
-    return copy;
 }
