05072900fa2910c0a32c693c10ccd94f758a0f16
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2022-12-11 10:34:45 +0300

    fix random

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "common.h"
 #include "Diff/diff.h"
@@ -8,6 +9,8 @@
 #include "Simplifyer/simpl.h"
 
 int main(int argc, const char **argv) {
+
+    srand((unsigned int)time(NULL));
 
     CLArgs filenames = get_filenames(argc, argv);
 
