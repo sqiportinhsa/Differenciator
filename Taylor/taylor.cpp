b9657aa4480911eef8b8cc197bf7aa8a0c51e6e1
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2022-12-16 09:11:36 +0300

    fix codestyle

--- Taylor/taylor.cpp
+++ Taylor/taylor.cpp
@@ -19,7 +19,9 @@ void taylor(Expression *expr) {
                    "Сначала нам потребуется вычислить оставшиеся производные вплоть до %d-го "
                    "порядка.\n\n", num_of_derivatives);
 
-    expr->taylor.head = Add(copy_subtree(expr->origin.head), Mul(Div(copy_subtree(expr->derivative.head), Const(1)), Sub(Variable('y'), Variable('x'))));
+    expr->taylor.head = Add(copy_subtree(expr->origin.head), 
+                            Mul(Div(copy_subtree(expr->derivative.head), Const(1)), 
+                                Sub(Variable('y'), Variable('x'))));
 
     for (int i = 2; i <= num_of_derivatives; ++i) {
         calc_derivative(&expr->derivative, &expr->taylor, i);
@@ -49,7 +51,9 @@ static void calc_derivative(Tree *prev_der, Tree *taylor, int i) {
 
     simplify_tree(&next_der);
 
-    taylor->head = Add(taylor->head, Mul(Div(copy_subtree(next_der.head), Const(factorial(i))), Deg(Sub(Variable('y'), Variable('x')), Const(i))));
+    taylor->head = Add(taylor->head, 
+                       Mul(Div(copy_subtree(next_der.head), Const(factorial(i))), 
+                           Deg(Sub(Variable('y'), Variable('x')), Const(i))));
 
     free_node(prev_der->head);
     prev_der->head = copy_subtree(next_der.head);
